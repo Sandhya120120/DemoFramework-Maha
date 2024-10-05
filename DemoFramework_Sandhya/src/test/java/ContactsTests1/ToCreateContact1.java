109968bb09943e1fc6e1cd57580ef8352902eb66
LENOVO <LENOVO@DESKTOP-SN4Q53J>
2024-10-04 13:40:14 -1200

    Pushing the project

---
+++ DemoFramework_Sandhya/src/test/java/ContactsTests1/ToCreateContact1.java
@@ -0,0 +1,8 @@
+package ContactsTests1;
+
+public class ToCreateContact1 {
+	public static void main(String[] args) {
+		System.out.println("Created Contacts Successfully");
+	}
+
+}
---
+++ DemoFramework_Sandhya/src/test/java/OrganizationsTest1/ToCreateOrganizationTest1.java
@@ -0,0 +1,8 @@
+package OrganizationsTest1;
+
+public class ToCreateOrganizationTest1 {
+
+	public static void main(String[] args) {
+		System.out.println("Created Organizations Successfully");
+	}
+}
